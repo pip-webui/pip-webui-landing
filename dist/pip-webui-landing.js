55ff85e70333e8fe6e926f75500071d52ac44bd1
Sergey Seroukhov <seroukhov@gmail.com>
2016-10-20 15:25:48 -0500

    Rebuilt

---
+++ dist/pip-webui-landing.js
@@ -0,0 +1,17 @@
+/**
+ * @file Registration of landing WebUI controls
+ * @copyright Digital Living Software Corp. 2014-2016
+ */
+
+/* global angular */
+
+(function (angular) {
+    'use strict';
+
+    angular.module('pipLanding', [
+    ]);
+
+})(window.angular);
+
+
+//# sourceMappingURL=pip-webui-landing.js.map
