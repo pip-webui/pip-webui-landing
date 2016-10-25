3343b12bf4dc4972b25c3000be71d68b5b13f814
Sergey Seroukhov <seroukhov@gmail.com>
2016-10-24 22:12:12 -0500

    Fixed build in behaviors

--- dist/pip-webui-landing.js
+++ dist/pip-webui-landing.js
@@ -1,17 +1,8 @@
-/**
- * @file Registration of landing WebUI controls
- * @copyright Digital Living Software Corp. 2014-2016
- */
-
-/* global angular */
-
 (function (angular) {
     'use strict';
-
-    angular.module('pipLanding', [
-    ]);
-
+    angular.module('pipLanding', []);
 })(window.angular);
+
 
 
 //# sourceMappingURL=pip-webui-landing.js.map
