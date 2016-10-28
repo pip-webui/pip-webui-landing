d23d4283837e669380741677d2f32d697a5e5089
Sergey Seroukhov <seroukhov@gmail.com>
2016-10-27 23:42:24 -0500

    Added pipNavService

--- dist/pip-webui-landing.js
+++ dist/pip-webui-landing.js
@@ -1,8 +1,3 @@
-/**
- * @file Registration of landing WebUI controls
- * @copyright Digital Living Software Corp. 2014-2016
- */
-/* global angular */
 (function (angular) {
     'use strict';
     angular.module('pipLanding', []);
